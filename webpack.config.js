056e4d88f6dac4df9aa84596c7353180adea61da
chjxx <19616414@qq.com>
2018-04-26 13:15:04 +0800

    config the weback

--- webpack.config.js
+++ webpack.config.js
@@ -9,34 +9,43 @@ const SRC_PATH = path.resolve('./app');
 // 打包后的资源目录
 const ASSETS_BUILD_PATH = path.resolve('./build');
 
+
 module.exports = {
   context: SRC_PATH, // 设置源代码的默认根路径
+  resolve: {
+    extensions:['.js', 'jsx'] // 同时支持js和jsx
+  },
   entry: {
     index: './index'
   },
   output: {
     path: ASSETS_BUILD_PATH,
-    filename: './[name].js'
+    filename: 'js/[name].js'
   },
   module: {
     rules:
     [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          query: {
+          options: {
             presets: ["env", "stage-0"]
           }
-        }
+        },
+        exclude: /node_modules/
       },
       {
         test: /(\.scss|\.sass)$/,
         use: ExtractTextPlugin.extract({
           use: [
             'css-loader',
-            'postcss-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')]
+              }
+            },
             'sass-loader'
           ]
         }),
@@ -54,14 +63,12 @@ module.exports = {
     new CleanWebpackPlugin([ASSETS_BUILD_PATH], {verbose: false}),
     new HtmlWebpackPlugin({
       template: SRC_PATH + "/index.html",
-      filename:'index.html',
-      hash:true,
-      minify:{ removeAttributeQuotes:true }
+      filename: 'index.html',
+      hash: true,
+      minify: { removeAttributeQuotes:true }
     }),
     // 抽取 CSS 文件
-    new ExtractTextPlugin({
-      filename: '[name].css'
-    }),
+    new ExtractTextPlugin('css/[name].css'),
     new webpack.HotModuleReplacementPlugin()
   ]
 };
