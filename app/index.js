9de6cd608dc11cccc5659360cccac48fb5493b9b
chjxx <19616414@qq.com>
2018-04-27 19:06:09 +0800

    Wrote the javascript .e.g

--- app/index.js
+++ app/index.js
@@ -1,3 +1,13 @@
 import './index.scss';
 
-let html = document.body;
+class Game{
+  constructor(id = '#game'){
+    this.$container = document.querySelector(id);
+    this.$currScore = document.querySelector('.current .number');
+    this.$bestScore = document.querySelector('.best .number');
+    this.$restartBtn = document.querySelector('.info .restart');
+    this.$piecesLevel = document.querySelector('.board .pieces');
+  }
+}
+
+let game = new Game();
