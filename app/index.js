fcd65e9e359e6608258ea18da95aef2bb0eb89c0
chjxx <19616414@qq.com>
2018-04-28 11:02:07 +0800

    wrote the javascript

--- app/index.js
+++ app/index.js
@@ -1,5 +1,12 @@
 import './index.scss';
 
+let boardData = [
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0]
+]
+
 class Game{
   constructor(id = '#game'){
     this.$container = document.querySelector(id);
@@ -7,7 +14,29 @@ class Game{
     this.$bestScore = document.querySelector('.best .number');
     this.$restartBtn = document.querySelector('.info .restart');
     this.$piecesLevel = document.querySelector('.board .pieces');
+    const keyFunc = [this.goLeft, this.goUp, this.goRight, this.goDown];
+    const keyCodes = [37, 38, 39, 40];
+
+    document.body.addEventListener('keydown', (e) => {
+      let idx = keyCodes.indexOf(e.keyCode)
+      if(idx !== -1){
+        keyFunc[idx]();
+      }
+    })
+  }
+  goUp(){
+    console.log('up');
+  }
+  goDown(){
+    console.log('down');
+  }
+  goLeft(){
+    console.log('left');
+  }
+  goRight(){
+    console.log('right');
   }
 }
 
 let game = new Game();
+
